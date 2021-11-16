d84670b2bf8d75e0b75cacd8e16c9de6a0fa96fb
Ryan Hagelstrom <rhagelstrom@atcorp.com>
2021-11-15 18:33:19 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Effective NPC
+
+Effective NPC is an 5E extension that enables effects in the custom effects list to be added to the NPC sheet by adding the text "has become customEffectNameTag"
+
+##Example
+Add a custom effect
+Engulfed; Restrained; SDMGOS: 6d6 acid
+
+Modify NPC sheet and add "has become engulfed" in the npc action or traits
+
+Add NPC to combat tracker
+
+The engulfed effect can now be applied from the NPC sheet
+
+<a href="https://www.fantasygrounds.com/forums/showthread.phptarget="_blank">Comments and Bug Reports</a>
---
+++ scripts/manager_power_NPC.lua
@@ -0,0 +1,71 @@
+--  	Author: Ryan Hagelstrom
+--	  	Copyright © 2021
+--	  	This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
+--	  	https://creativecommons.org/licenses/by-sa/4.0/
+local parseEffects = nil
+
+function onInit()
+	parseEffects = PowerManager.parseEffects
+	PowerManager.parseEffects = customParseEffect
+end
+
+
+function customParseEffect(sPowerName, aWords)
+	local effects = parseEffects(sPowerName,aWords)
+	local sMatch = nil;
+	local rCurrent = nil;
+	local sPower = table.concat(aWords, " ")
+	local bValid = false
+
+	-- Do we have the key phrase "has become"
+	if (sPower:match("has become")) then
+		--Find the effect name in our custom effects list
+		for _,v in pairs(DB.getChildrenGlobal("effects")) do
+			rEffect = {}
+			local sEffect = DB.getValue(v, "label", "")
+			if sEffect ~= nil and sEffect ~= "" then
+				aEffectComps = EffectManager.parseEffect(sEffect)
+				-- Is this the effeect we are looking for?
+				-- Name is parsed to index 1 in the array
+				local sMatchString = "has become " .. aEffectComps[1]:lower()
+				if sPower:match(sMatchString) then
+					local i,j,k = 1
+					local aEffectWords = {}
+					
+					for word in aEffectComps[1]:lower():gmatch("%w+") do table.insert(aEffectWords, word) end
+					for i = 1, #aWords do
+						bValid = true
+						if StringManager.isWord(aWords[i], "has") and StringManager.isWord(aWords[i+1], "become") then
+							j = i+2
+							for k=1, #aEffectWords do
+								if( aEffectWords[k] ~= aWords[j]) then
+									bValid = false
+									break
+								end
+								j = j + 1
+							end
+							if bValid == true then
+								local rCurrent = {}
+								rCurrent.sName = sEffect
+								rCurrent.startindex = i
+								j=j-1
+								rCurrent.endindex = j
+								rCurrent.nGMOnly = tonumber(DB.getChild(v, "isgmonly"))
+								rCurrent.nDuration = tonumber(DB.getChild(v, "duration"))
+								rCurrent.sUnits = DB.getChild(v, "unit")
+								PowerManager.parseEffectsAdd(aWords, i, rCurrent, effects)
+
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+	return effects
+end
+
+-- Set things back the way they origiinally were
+function onClose()
+	PowerManager.parseEffects = parseEffects
+end
